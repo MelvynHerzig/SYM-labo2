bd58d5117272df7ac0bb338e3bd3e8f3faca3ab0
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-11-16 16:52:48 +0100

    De la doc

--- Labo2/app/src/main/java/ch/heigvd/iict/sym/labo2/comm/CommunicationEventListener.kt
+++ Labo2/app/src/main/java/ch/heigvd/iict/sym/labo2/comm/CommunicationEventListener.kt
@@ -1,13 +1,10 @@
-/**
- * @author Berney Alec
- * @author Forestier Quentin
- * @author Herzig Melvyn
- */
-
 package ch.heigvd.iict.sym.lab.comm
 
 /**
  * Interface utilisé pour gérer différents types de réponse du serveur
+ * @author Berney Alec
+ * @author Forestier Quentin
+ * @author Herzig Melvyn
  */
 interface CommunicationEventListener {
     fun handleServerResponse(response : ByteArray)
